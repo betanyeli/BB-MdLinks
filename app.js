c40d40ce22a0d1483b3ba2fee9da4dd092f50f7a
betanyeli <betangelii@gmail.com>
2019-06-19 17:42:28 -0400

    filehound create

--- app.js
+++ app.js
@@ -1,4 +1,49 @@
-const fs = require('fs');
-let readMe = fs.readFileSync('README.md', 'utf8');
-console.log(readMe);
-//testing lectura de archivo MD
+const fs = require('fs'); // leo los archivos
+const marked = require('marked'); // extraigo los links y los meto en array
+//const route = require('path'); // ruta de mi archivo
+const FileHound = require('filehound');
+
+ 
+//  is absolute method y resolve pa convdertir en absolute, process argv
+
+// Paso como parámetro la ruta absoluta donde buscará
+const links = (path) =>{
+      fs.readFile(path,"utf8", (err,data) =>{ // quita esto de aquí
+        if(err){
+            throw err;
+          }
+          console.log(data);
+          let links =[];
+      
+          const renderer = new marked.Renderer();
+      
+          renderer.link = function(href, title, text){
+      
+            links.push({
+              
+              href:href,
+              text:text,
+              file:path
+            
+            })
+      
+          }
+          marked(data, {renderer:renderer})
+            console.log(links)
+    }) // quita esto de aqui
+    
+    }
+
+  
+  console.log(links('./prueba.md'));
+//console.log(links('./testing'));
+ //console.log(route.extname('./prueba.md') );
+// Returns: '.md'
+
+ 
+const files = FileHound.create() //sólo lee directorios
+  .paths('./testing')
+  .ext('md')
+  .find();
+ 
+files.then(console.log);
---
+++ testing/pruebados.md
@@ -0,0 +1,1 @@
+www.feisbuck.com
