e0f85ed81edfca09ab7330332e7ff30137e38589
betanyeli <betangelii@gmail.com>
2019-06-21 16:56:08 -0400

    otra vez sin nada nuevo

--- app.js
+++ app.js
@@ -2,25 +2,26 @@ const fs = require('fs'); // leo los archivos
 const marked = require('marked'); // extraigo los links y los meto en array
 //const route = require('path'); // ruta de mi archivo
 const fileHound = require('filehound');
-
+//let route = process.argv[2] //route
+// let optionUser = process.argv[3] testing
 
  
 //  is absolute method y resolve pa convdertir en absolute, process argv
 
 // Paso como parámetro la ruta absoluta donde buscará
-const links = (path) =>{
+const readLinks = (path) =>{
       fs.readFile(path,"utf8", (err,data) =>{ // quita esto de aquí
         if(err){
             throw err;
           }
-          console.log(data);
-          let links =[];
+          console.log("Solo los links de ese archivo", data);
+          let readLinks =[];
       
           const renderer = new marked.Renderer();
       
           renderer.link = function(href, title, text){
       
-            links.push({
+            readLinks.push({
               
               href:href,
               text:text,
@@ -30,28 +31,26 @@ const links = (path) =>{
       
           }
           marked(data, {renderer:renderer})
-            console.log(links)
-    }) // quita esto de aqui
+          console.log("Links pusheados en array", readLinks)
+          
+    }) 
+   
+        } //Fin función readLinks
+    console.log("links2", readLinks('./prueba.md'));
     
-    }
-
-  
-  console.log(links('./prueba.md'));
-  
-  console.log(process.argv);
-  process.argv.forEach((val, index) => {
-console.log(`${index}: ${val}`);
-  })
-//console.log(links('./testing'));
- //console.log(route.extname('./prueba.md') );
-// Returns: '.md'
 
  //función que me extraerá los md de un directorio
-const files = 
+const readDirectory = 
     fileHound.create() //sólo lee directorios
+    .discard('node_modules')
     .paths('./testing')
     .ext('md')
     .find();
-    files.then(console.log)
+    readDirectory.then(console.log)
 
+//Process argv
+console.log("process arvg", process.argv);
+process.argv.forEach((val, index) => {
+console.log( "arvg", `${index}: ${val}`);
 
+})
