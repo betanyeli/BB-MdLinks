d42defc8b544a79d754b64e8ae595876febccfc2
betanyeli <betangelii@gmail.com>
2019-06-25 17:08:15 -0400

    capturar valor de path or file en consola

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
 #!/usr/bin/env node
-//require('../')()
 const fs = require('fs'); // leo los archivos
 const marked = require('marked'); // extraigo los links y los meto en array
-const route = require('path'); // ruta de mi archivo
-const fileHound = require('filehound');
-//let route = process.argv[2] //route
-// let optionUser = process.argv[3] testing
+//const route = require('path'); // ruta de mi archivo
+const fileHound = require('filehound'); // read directory
+const fetch = require('node-fetch'); //links status
+let command = []; //array process vacío.
+let links = [];
 
- 
-//  is absolute method y resolve pa convdertir en absolute, process argv
+//Process argv
+
+//console.log("process arvg", process.argv);
+process.argv.forEach((val, index) => {
+  command.push(process.argv[index])
+})
+console.log("command", command);
+
+//process arvg position
+
+fs.stat(command[2],(error, stats) =>{
+
+  if (error){
+console.log("error stats", error);
+} if (stats.isFile()) {
+  readLinks(command[2]);
+} if (stats.isDirectory()){
+  readRoute(command[2]);
+}
+console.log("isFile", stats.isFile());
+console.log("isDirectory", stats.isDirectory());
+
+
+  });
 
 // Paso como parámetro la ruta absoluta donde buscará
-const readLinks = (path) =>{
-      fs.readFile(path,"utf8", (err,data) =>{ // quita esto de aquí
+const readLinks = (files) =>{
+      fs.readFile(files,"utf8", (err,data) =>{ // quita esto de aquí
         if(err){
             throw err;
           }
           console.log("Solo los links de ese archivo", data);
-          let readLinks =[];
+          //let links = [];
       
           const renderer = new marked.Renderer();
       
           renderer.link = function(href, title, text){
       
-            readLinks.push({
+            links.push({
               
               href:href,
               text:text,
@@ -33,31 +55,49 @@ const readLinks = (path) =>{
       
           }
           marked(data, {renderer:renderer})
-          console.log("Links pusheados en array", readLinks)
+          console.log("Links pusheados en array", links)
           
+          scanLinks(links)
     }) 
-   
+    
         } //Fin función readLinks
-    console.log("readLinksConsole", readLinks('./prueba.md'));
+   //console.log("readLinksConsole", readLinks('./prueba.md')); este console no va
+    
     
 
  //función que me extraerá los md de un directorio
-const readDirectory = 
-    fileHound.create() //sólo lee directorios
-    .discard('node_modules')
-    .paths('./testing')
-    .ext('md')
-    .find();
-    readDirectory.then(console.log)
-
-//Process argv
-console.log("process arvg", process.argv);
-process.argv.forEach((val, index) => {
-console.log( "arvg", `${index}: ${val}`);
-
-})
-
-const mdLinks = (path, options) => {
-
+const readRoute = (route) => {
+  const readDirectory = 
+  fileHound.create() //sólo lee directorios
+  .discard('node_modules')
+  .paths(route)
+  .ext('md')
+  .find();
+ 
+  readDirectory.then((marico) => console.log(marico));
 }
-module.exports = mdLinks;
+    
+
+
+// fetch
+const scanLinks = (url) => {
+  url.forEach(element => {
+    fetch(element.href)
+    .then(res => {
+      console.log("true or false", res.ok);
+      console.log("number", res.status);
+      console.log("ok or not found", res.statusText);
+     
+  });
+  })
+
+  
+}
+  
+
+  
+
+// const mdLinks = (path, options) => {
+
+// }
+// module.exports = mdLinks;
