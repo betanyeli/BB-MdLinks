2e39bf85c5a1c70ea7e1b7e698ee8024425bbe35
betanyeli <betangelii@gmail.com>
2019-06-29 01:05:44 -0400

    f(mdLinks) export to index.js, only works with path, option in progress

--- md-links.js
+++ md-links.js
@@ -1,27 +1,96 @@
-const mdLinks= (path, options) => {
-    return new Promise ((resolve, reject) => {
-        fs.stat(path, (error, stats) => {
+#!/usr/bin/env node
+const fs = require('fs'); // leo los archivos
+const marked = require('marked'); // extraigo los links y los meto en array
+const fileHound = require('filehound'); // read directory
+const path = require('path');
+const fetch = require('node-fetch'); //links status
+let colors = require('colors');
+let links = [];
 
-            if (error) {
-           return reject(error)
-            } if (stats.isFile()) {
-            resolve(readLinks)
-            } if (stats.isDirectory()) {
-            resolve(readRoute);
-            }
-        //   console.log("isFile", stats.isFile());
-        //   console.log("isDirectory", stats.isDirectory());
-        });
-        if(options.validate){
-            return validateLinks(path)
-          }
-        // if(options.stats){
-        //     return statsLinks(path)
-        //   }
-    })
-    // if(options.stats && options.validate){
-    //     return promiseAllDe validate y stats?
-    //   }  
+/*Si es un archivo o ruta, llama a la función correspondiente*/ 
+const mdLinks = (path) => {
+  fs.stat(path, (error, stats) => {
+    if (error) {
+    console.log("error stats".red, error);
+    } if (stats.isFile()) {
+    readLinks(path);
+    } if (stats.isDirectory()) {
+    readRoute(path);
+    }
+});
+//Acá va la lógica de las options
 
 }
-module.exports = mdLinks;
+
+
+/*F(x) para leer links convertida en promesa */ 
+const readLinks = (files) => {
+  return new Promise ((resolve, reject) => {
+
+  fs.readFile(files, "utf8", (err, data) => {
+  
+    err ? reject(err) : resolve(links); 
+   //console.log("Solo los links de ese archivo", data);
+
+    const renderer = new marked.Renderer();
+
+    renderer.link = function (href, title, text) {
+
+      links.push({
+        href: href,
+        text: text,
+        title:title
+       
+
+      });
+
+    } //fin rendered
+    marked(data, { renderer: renderer })
+    validateLinks(links)
+    //resolve(links)
+  }) //fin readFiles
+  })
+ 
+} //Fin función readLinks
+
+
+/*F(x) que lee directorios, posteriormente llama a readLinks*/
+const readRoute = (route) => {
+    const readDirectory = 
+        fileHound.create() //sólo lee directorios
+        .discard('node_modules')
+        .paths(route)
+        .ext('md')
+        .find()
+            readDirectory.then(read => {
+                read.forEach((file) =>{
+                    readLinks(file) // Llamo a readlinks
+            })
+        })
+            .catch(err=>{
+            console.log(err)
+    })
+
+}
+
+
+// fetch
+const validateLinks = (url) => {
+  //urlArray
+ url.forEach(element => {
+   //urlObject
+   fetch(element)
+     .then(res => {
+       let urlArray = []; //Array de links del fetch
+       let urlObject = {}; //Objects del array de links del fetch
+       urlObject.url = res.url;
+       urlObject.ok = res.ok;
+       urlObject.status = res.status;
+       urlObject.statusText = res.statusText;
+       urlArray.push(urlObject);
+       console.log(urlArray)
+     })     
+ }); // fin foreach
+}//fin función validateLinks
+
+module.exports= mdLinks;
--- testing/prueba2.md
+++ testing/prueba2.md
@@ -1,3 +1,2 @@
 [Node.js](https://nodejs.org/)
-[Markdown](https://es.wikipedia.org/wiki/Markdown)
-[http.get](https://nodejs.org/api/http.html#http_http_get_options_callback)
+[http.get](https://facebook.com)
